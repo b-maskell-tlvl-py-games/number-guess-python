fcc9a32822deb02134bc6f70073571334d481688
b-maskell <brian.maskell.uk@gmail.com>
2023-07-04 21:16:09 +0100

    added comments

--- NumberGuess.py
+++ NumberGuess.py
@@ -2,13 +2,20 @@ import random
 
 
 def run_game():
-  number_guesses = 0
-  target_num = random.randint(1,99)
+  '''
+  Run number guess game, where a random number is generated and
+  the number of guesses it takes for the player to identify it
+  is counted.
+  '''
+
+  number_guesses = 0                  # track num of player guesses
+  target_num = random.randint(1,99)   # generate rnd number to guess
 
   while True:
     number_guesses += 1
     player_guess = int( input("Enter Guess (1-99): "))
 
+    # Compare players guess to the rnd number and give player feedback
     if player_guess == target_num:
       print(f"Well Done {player_guess} was Correct! - number of guesses was {number_guesses}")
       break
@@ -21,7 +28,8 @@ def run_game():
 
 # MAIN IDEOM - only run code below if called as script
 if __name__ == '__main__':
-  
+
+  # Keep running game until player quits  
   while True:
     run_game()
 
